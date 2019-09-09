4084c6091fba9662dae3aeb9eebbba556329d69e
Mohamed isse <mail@annabengtsson.com>
2019-09-09 10:07:26 +0200

    Added files

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var knappen = document.getElementById("knapp");
+
+knappen.addEventListener("click", function() {
+var txten = document.getElementById("txt");
+txten.value = txten.value.toUpperCase();
+});
